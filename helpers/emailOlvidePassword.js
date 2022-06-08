a454d85696b5761fe1088f05fa5b1b7023a22677
jhon11390 <meljhon2@gmail.com>
2022-06-08 14:14:42 -0500

    proyecto express v1

---
+++ helpers/emailOlvidePassword.js
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer'
+
+const emailOlvidePaasword = async (datos) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  const {email, nombre, token} = datos
+  
+  const info = await transporter.sendMail({
+    from: 'APV- Administrador de pacientes de veterinaria',
+    to: email,
+    subject: "Reestablece tu password",
+    text: "Reestablece tu password",
+    html: `<p>Hola: ${nombre}, has solicitado reestablecer tu password.</P>
+          <p>Sigue el  siguiente enlace para generar un nuevo password:
+          <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">Reestablecer password</a></p>
+
+          <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
+    `
+  })
+  console.log("Mensaje enviado: %s", info.messageId);
+}
+
+export default emailOlvidePaasword;
